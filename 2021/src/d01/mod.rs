c97726ba23022f586dc3ee54be748e1a371ca30d
Stuart Murray <10003354+stoovon@users.noreply.github.com>
2023-12-10 21:27:39 +0000

    feat: 2021d01 solve

--- 2021/src/d01/mod.rs
+++ 2021/src/d01/mod.rs
@@ -1,31 +1,48 @@
 extern crate core;
 
-pub fn fn1(_input: &str) -> i64 {
-    todo!()
+pub fn fn1(input: &str) -> i64 {
+    let data: Vec<i64> = input
+        .lines()
+        .map(|v| v.parse::<i64>().ok().unwrap_or_default())
+        .collect();
+
+    data.windows(2).filter(|d| d[0] < d[1]).count().try_into().unwrap()
 }
 
-pub fn fn2(_input: &[&str]) -> i64 {
-    todo!()
+pub fn fn2(input: &str) -> i64 {
+    let data: Vec<i64> = input
+        .lines()
+        .map(|v| v.parse::<i64>().ok().unwrap_or_default())
+        .collect();
+
+    data.windows(3)
+        .zip(data.windows(3).skip(1))
+        .filter(|(a, b)| a.iter().sum::<i64>() < b.iter().sum())
+        .count().try_into().unwrap_or_default()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use svutils::load_spec;
-    use itertools::Itertools;
 
     #[test]
-    fn test_example() {
-        assert_eq!(fn1(include_str!("example.txt")), load_spec(include_str!("example-spec.txt")));
+    fn test_fn1_example() {
+        assert_eq!(fn1(include_str!("example.txt")), load_spec(include_str!("example-spec.1.txt")));
     }
 
     #[test]
-    fn test_fn1_test_case_name() {
+    fn test_fn1_input() {
         assert_eq!(fn1(include_str!("input.txt")), load_spec(include_str!("input-spec.1.txt")));
     }
 
     #[test]
-    fn test_fn2_test_case_name() {
-        assert_eq!(fn2(&include_str!("input.txt").lines().collect_vec()), load_spec(include_str!("input-spec.2.txt")));
+    fn test_fn2_example() {
+        assert_eq!(fn2(include_str!("example.txt")), load_spec(include_str!("example-spec.2.txt")));
+    }
+
+    #[test]
+    fn test_fn2_input() {
+        assert_eq!(fn2(include_str!("input.txt")), load_spec(include_str!("input-spec.2.txt")));
     }
 }

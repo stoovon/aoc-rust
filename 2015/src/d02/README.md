fa86f050f3582c95e3543e156829ab26edf601c1
Stuart Murray <10003354+stoovon@users.noreply.github.com>
2023-12-10 20:18:52 +0000

    feat: 2015d02 solve

--- 2015/src/d02/README.md
+++ 2015/src/d02/README.md
@@ -11,3 +11,5 @@ These can be downloaded and put into `example.txt`, `example-spec.txt`, `input.t
 The expectation is that the spec files will contain a single number, which is the expected answer.
 
 ## Solution Sketch / Solving Notes
+
+- Can probably significantly shorten the solution but I wanted to see how plainly/obviously I could express things with typing.
--- 2015/src/d02/mod.rs
+++ 2015/src/d02/mod.rs
@@ -1,31 +1,78 @@
 extern crate core;
 
-pub fn fn1(_input: &str) -> i64 {
-    todo!()
+#[derive(Debug)]
+struct Parcel {
+    w: i64,
+    d: i64,
+    h: i64,
 }
 
-pub fn fn2(_input: &[&str]) -> i64 {
-    todo!()
+fn parse_parcel(s: &str) -> Option<Parcel> {
+    let mut parts: Vec<i64> = s.split('x').filter_map(|part| part.parse().ok()).collect();
+    parts.sort_unstable();
+    if parts.len() == 3 {
+        Some(Parcel {
+            w: parts[0],
+            d: parts[1],
+            h: parts[2],
+        })
+    } else {
+        None
+    }
+}
+
+fn parse_the_parcels(input: &str) -> Vec<Parcel> {
+    input
+        .lines()
+        .map(|l| parse_parcel(l))
+        .filter_map(|p| p)
+        .collect()
+}
+
+pub fn fn1(input: &str) -> i64 {
+    parse_the_parcels(input)
+        .iter()
+        .map(|parcel| {
+            2 * (parcel.w * parcel.d + parcel.w * parcel.h + parcel.d * parcel.h) + 
+            parcel.w * parcel.d
+        })
+        .sum()
+}
+
+pub fn fn2(input: &str) -> i64 {
+    parse_the_parcels(input)
+        .iter()
+        .map(|parcel| {
+            // Potentially gnarly, let's try this
+            2 * (parcel.w + parcel.d) +
+            // Cubic volume
+            parcel.w * parcel.d * parcel.h
+        })
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use svutils::load_spec;
-    use itertools::Itertools;
 
     #[test]
-    fn test_example() {
-        assert_eq!(fn1(include_str!("example.txt")), load_spec(include_str!("example-spec.txt")));
+    fn test_fn1_example() {
+        assert_eq!(fn1(include_str!("example.txt")), load_spec(include_str!("example-spec.1.txt")));
     }
 
     #[test]
-    fn test_fn1_test_case_name() {
+    fn test_fn1_input() {
         assert_eq!(fn1(include_str!("input.txt")), load_spec(include_str!("input-spec.1.txt")));
     }
 
     #[test]
-    fn test_fn2_test_case_name() {
-        assert_eq!(fn2(&include_str!("input.txt").lines().collect_vec()), load_spec(include_str!("input-spec.2.txt")));
+    fn test_fn2_example() {
+        assert_eq!(fn2(include_str!("example.txt")), load_spec(include_str!("example-spec.2.txt")));
+    }
+
+    #[test]
+    fn test_fn2_input() {
+        assert_eq!(fn2(include_str!("input.txt")), load_spec(include_str!("input-spec.2.txt")));
     }
 }

9d7edd58d1add764cfd9e3b39428f2358429e4ae
Stuart Murray <10003354+stoovon@users.noreply.github.com>
2023-12-26 22:52:34 +0000

    tests: ignore not-yet-implemented tests

--- 2020/src/d13/mod.rs
+++ 2020/src/d13/mod.rs
@@ -17,21 +17,25 @@ mod tests {
     const DAY: i16 = 13;
 
     #[test]
+    #[ignore]
     fn test_fn1_example() {
         scaffold_test(YEAR, DAY, "example.txt", "example-spec.1.txt", fn1);
     }
 
     #[test]
+    #[ignore]
     fn test_fn1_input() {
         scaffold_test(YEAR, DAY, "input.txt", "input-spec.1.txt", fn1);
     }
 
     #[test]
+    #[ignore]
     fn test_fn2_example() {
         scaffold_test(YEAR, DAY, "example.txt", "example-spec.2.txt", fn2);
     }
 
     #[test]
+    #[ignore]
     fn test_fn2_input() {
         scaffold_test(YEAR, DAY, "input.txt", "input-spec.2.txt", fn2);
     }

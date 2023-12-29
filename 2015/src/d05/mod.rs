184c3146b3db3e46a7289f6294e85680ae211e7f
Stuart Murray <10003354+stoovon@users.noreply.github.com>
2023-12-29 10:54:56 +0000

    feat: AoC2015d05

--- 2015/src/d05/mod.rs
+++ 2015/src/d05/mod.rs
@@ -1,11 +1,36 @@
 extern crate core;
 
-pub fn fn1(_input: &str) -> i64 {
-    todo!()
+use fancy_regex::Regex;
+
+pub fn fn1(input: &str) -> i64 {
+    input.lines().filter(|line| {
+        let mut vowels = 0;
+        let mut double = false;
+        let mut bad = false;
+        let mut last = ' ';
+        for c in line.chars() {
+            if c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' {
+                vowels += 1;
+            }
+            if c == last {
+                double = true;
+            }
+            if (last == 'a' && c == 'b') || (last == 'c' && c == 'd') || (last == 'p' && c == 'q') || (last == 'x' && c == 'y') {
+                bad = true;
+            }
+            last = c;
+        }
+        vowels >= 3 && double && !bad
+    }).count() as i64
 }
 
-pub fn fn2(_input: &str) -> i64 {
-    todo!()
+pub fn fn2(input: &str) -> i64 {
+    let re1 = Regex::new(r"(..).*\1").unwrap();
+    let re2 = Regex::new(r"(.).\1").unwrap();
+
+    input.lines().filter(|line| {
+        re1.is_match(line).unwrap() && re2.is_match(line).unwrap()
+    }).count() as i64
 }
 
 #[cfg(test)]
@@ -17,25 +42,21 @@ mod tests {
     const DAY: i16 = 5;
 
     #[test]
-    #[ignore]
     fn test_fn1_example() {
         scaffold_test(YEAR, DAY, "example.txt", "example-spec.1.txt", fn1);
     }
 
     #[test]
-    #[ignore]
     fn test_fn1_input() {
         scaffold_test(YEAR, DAY, "input.txt", "input-spec.1.txt", fn1);
     }
 
     #[test]
-    #[ignore]
     fn test_fn2_example() {
         scaffold_test(YEAR, DAY, "example.txt", "example-spec.2.txt", fn2);
     }
 
     #[test]
-    #[ignore]
     fn test_fn2_input() {
         scaffold_test(YEAR, DAY, "input.txt", "input-spec.2.txt", fn2);
     }
--- input/2015/d05/README.md
+++ input/2015/d05/README.md
@@ -14,6 +14,11 @@ A nice string is one with all of the following properties:
 - It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
 - It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
 
+2. How many strings are nice (revised method):
+
+- It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+- It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+
 ## Input files
 
 These can be downloaded and put into e.g.

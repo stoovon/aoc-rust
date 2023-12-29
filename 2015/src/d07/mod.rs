c8fca0a0d0730c48b70e0a721e5d5ffe7dbe6b65
Stuart Murray <10003354+stoovon@users.noreply.github.com>
2023-12-29 12:48:05 +0000

    feat: AoC2015d07

--- 2015/src/d07/mod.rs
+++ 2015/src/d07/mod.rs
@@ -1,11 +1,89 @@
 extern crate core;
 
-pub fn fn1(_input: &str) -> i64 {
-    todo!()
+use std::collections::HashMap;
+
+type Result = (u16, u16);
+
+enum Gate<'a> {
+    Wire(&'a str),
+    Not(&'a str),
+    And(&'a str, &'a str),
+    Or(&'a str, &'a str),
+    LeftShift(&'a str, u16),
+    RightShift(&'a str, u16),
 }
 
-pub fn fn2(_input: &str) -> i64 {
-    todo!()
+pub fn parse(input: &str, chosen_wire: &str, override_wire: &str) -> Result {
+    let mut tokens = input.split_ascii_whitespace();
+    let mut circuit = HashMap::new();
+
+    while let (Some(first), Some(second)) = (tokens.next(), tokens.next()) {
+        let gate = if first == "NOT" {
+            let _third = tokens.next().unwrap();
+            Gate::Not(second)
+        } else if second == "->" {
+            Gate::Wire(first)
+        } else {
+            let third = tokens.next().unwrap();
+            let _fourth = tokens.next().unwrap();
+
+            match second {
+                "AND" => Gate::And(first, third),
+                "OR" => Gate::Or(first, third),
+                "LSHIFT" => Gate::LeftShift(first, third.parse::<u16>().unwrap()),
+                "RSHIFT" => Gate::RightShift(first, third.parse::<u16>().unwrap()),
+                _ => unreachable!(),
+            }
+        };
+
+        let wire = tokens.next().unwrap();
+        circuit.insert(wire, gate);
+    }
+
+    let mut cache = HashMap::new();
+    let result1 = signal(chosen_wire, &circuit, &mut cache);
+
+    cache.clear();
+    cache.insert(override_wire, result1);
+    let result2 = signal(chosen_wire, &circuit, &mut cache);
+
+    (result1, result2)
+}
+
+fn signal<'a>(
+    key: &'a str,
+    circuit: &HashMap<&'a str, Gate<'a>>,
+    cache: &mut HashMap<&'a str, u16>,
+) -> u16 {
+    if let Some(result) = cache.get(key) {
+        return *result;
+    }
+
+    let result = if key.chars().next().unwrap().is_ascii_digit() {
+        key.parse::<u16>().unwrap()
+    } else {
+        match circuit[key] {
+            Gate::Wire(w) => signal(w, circuit, cache),
+            Gate::Not(w) => !signal(w, circuit, cache),
+            Gate::And(l, r) => signal(l, circuit, cache) & signal(r, circuit, cache),
+            Gate::Or(l, r) => signal(l, circuit, cache) | signal(r, circuit, cache),
+            Gate::LeftShift(w, n) => signal(w, circuit, cache) << n,
+            Gate::RightShift(w, n) => signal(w, circuit, cache) >> n,
+        }
+    };
+
+    cache.insert(key, result);
+    result
+}
+
+pub fn fn1(input: &str, chosen_wire: &str, override_wire: &str) -> i64 {
+    let (result, _) = parse(input, chosen_wire, override_wire);
+    result as i64
+}
+
+pub fn fn2(input: &str, chosen_wire: &str, override_wire: &str) -> i64 {
+    let (_, result) = parse(input, chosen_wire, override_wire);
+    result as i64
 }
 
 #[cfg(test)]
@@ -17,26 +95,22 @@ mod tests {
     const DAY: i16 = 7;
 
     #[test]
-    #[ignore]
     fn test_fn1_example() {
-        scaffold_test(YEAR, DAY, "example.txt", "example-spec.1.txt", fn1);
+        scaffold_test(YEAR, DAY, "example.txt", "example-spec.1.txt", |input| {fn1(input, "h", "i")});
     }
 
     #[test]
-    #[ignore]
     fn test_fn1_input() {
-        scaffold_test(YEAR, DAY, "input.txt", "input-spec.1.txt", fn1);
+        scaffold_test(YEAR, DAY, "input.txt", "input-spec.1.txt", |input| {fn1(input, "a", "b")});
     }
 
     #[test]
-    #[ignore]
     fn test_fn2_example() {
-        scaffold_test(YEAR, DAY, "example.txt", "example-spec.2.txt", fn2);
+        scaffold_test(YEAR, DAY, "example.txt", "example-spec.2.txt", |input| {fn2(input, "h", "i")});
     }
 
     #[test]
-    #[ignore]
     fn test_fn2_input() {
-        scaffold_test(YEAR, DAY, "input.txt", "input-spec.2.txt", fn2);
+        scaffold_test(YEAR, DAY, "input.txt", "input-spec.2.txt", |input| {fn2(input, "a", "b")});
     }
 }

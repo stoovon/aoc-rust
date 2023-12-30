adf1c438aa3e82a10f1d9f878248e39f0762d2de
Stuart Murray <10003354+stoovon@users.noreply.github.com>
2023-12-30 13:54:04 +0000

    feat: AoC2023d25
    Slow (26s) but works

--- 2023/src/d25/mod.rs
+++ 2023/src/d25/mod.rs
@@ -1,10 +1,84 @@
 extern crate core;
 
-pub fn fn1(_input: &str) -> i64 {
+use std::collections::{BTreeMap, BTreeSet, VecDeque};
+
+fn parse(contents: &str) -> BTreeMap<String, Vec<String>> {
+    let mut graph: BTreeMap<String, Vec<String>> = BTreeMap::new();
+    for line in contents.lines() {
+        if let Some((from, to)) = line.split_once(": ") {
+            let source = from.to_string();
+            for target in to.split(' ') {
+                graph
+                    .entry(source.clone())
+                    .and_modify(|e| {
+                        e.push(target.to_string());
+                    })
+                    .or_insert(vec![target.to_string()]);
+                graph
+                    .entry(target.to_string())
+                    .and_modify(|e| {
+                        e.push(source.clone());
+                    })
+                    .or_insert(vec![source.clone()]);
+            }
+        }
+    }
+    graph
+}
+
+fn split(graph: &BTreeMap<String, Vec<String>>) -> (u32, u32) {
+    let mut first_group: u32 = 1;
+    let mut second_group: u32 = 0;
+    let mut components = graph.keys();
+    let first_component = components.next().unwrap();
+    for component in components {
+        let mut connections: u32 = 0;
+        let mut tested: BTreeSet<String> = BTreeSet::from([first_component.clone()]);
+        for starting_component in graph.get(first_component).unwrap() {
+            if *starting_component == *component {
+                connections += 1;
+                continue;
+            }
+            let mut seen: BTreeSet<String> = BTreeSet::new();
+            let mut q: VecDeque<(String, Vec<String>)> = VecDeque::new();
+            let mut found = false;
+            q.push_back((starting_component.clone(), vec![starting_component.clone()]));
+            while !q.is_empty() && !found && connections < 4 {
+                let (other_component, path) = q.pop_front().unwrap();
+                for c in graph.get(&other_component).unwrap() {
+                    if *component == *c {
+                        connections += 1;
+                        for p in path.iter() {
+                            tested.insert(p.clone());
+                        }
+                        found = true;
+                        break;
+                    }
+                    else if !seen.contains(c) && !path.contains(&c) && !tested.contains(c) {
+                        let mut new_path = path.clone();
+                        new_path.push(c.clone());
+                        q.push_back((c.clone(), new_path));
+                        seen.insert(c.clone());
+                    }
+                }
+            }
+        }
+        if connections >= 4 {
+            first_group += 1;
+        } else {
+            second_group += 1;
+        }
+    }
+    (first_group, second_group)
+}
+
+pub fn fn1(input: &str) -> i64 {
     // Build a list of connections
     // Initial solve in Python/networkx. Will try to implement the solution in Rust soon.
 
-    todo!()
+    let graph = parse(input);
+    let (first_group, second_group) = split(&graph);
+    (first_group * second_group) as i64
 }
 
 #[cfg(test)]
@@ -16,13 +90,6 @@ mod tests {
     const DAY: i16 = 25;
 
     #[test]
-    #[ignore]
-    fn test_fn1_example() {
-        scaffold_test(YEAR, DAY, "example.txt", "example-spec.1.txt", fn1);
-    }
-
-    #[test]
-    #[ignore]
     fn test_fn1_input() {
         scaffold_test(YEAR, DAY, "input.txt", "input-spec.1.txt", fn1);
     }

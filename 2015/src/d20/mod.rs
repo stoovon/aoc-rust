2d583f5231d12fed98d056164c9f899d5ee5fa21
Stuart Murray <10003354+stoovon@users.noreply.github.com>
2023-12-30 10:57:43 +0000

    feat: AoC2015d20

--- 2015/src/d20/mod.rs
+++ 2015/src/d20/mod.rs
@@ -1,11 +1,34 @@
 extern crate core;
 
-pub fn fn1(_input: &str) -> i64 {
-    todo!()
+pub fn solve(input: &str, presents_per_elf: usize, limit: usize) -> i64 {
+    let threshold = input.trim().parse::<usize>().unwrap();
+    let mut houses = vec![0; threshold / 10 + 1];
+
+    for elf in 1..houses.len(){
+        let mut loc = elf;
+        let mut delivered = 0;
+        while loc < houses.len()  && delivered < limit {
+            houses[loc] += elf * presents_per_elf;
+            loc += elf;
+            delivered += 1;
+        }
+    }
+
+    for (i, house) in houses.iter().enumerate() {
+        if *house >= threshold {
+            return i as i64;
+        }
+    }
+
+    panic!("No house found");
 }
 
-pub fn fn2(_input: &str) -> i64 {
-    todo!()
+pub fn fn1(input: &str) -> i64 {
+    solve(input, 10, usize::MAX)
+}
+
+pub fn fn2(input: &str) -> i64 {
+    solve(input, 11, 50)
 }
 
 #[cfg(test)]
@@ -17,25 +40,21 @@ mod tests {
     const DAY: i16 = 20;
 
     #[test]
-    #[ignore]
     fn test_fn1_example() {
         scaffold_test(YEAR, DAY, "example.txt", "example-spec.1.txt", fn1);
     }
 
     #[test]
-    #[ignore]
     fn test_fn1_input() {
         scaffold_test(YEAR, DAY, "input.txt", "input-spec.1.txt", fn1);
     }
 
     #[test]
-    #[ignore]
     fn test_fn2_example() {
         scaffold_test(YEAR, DAY, "example.txt", "example-spec.2.txt", fn2);
     }
 
     #[test]
-    #[ignore]
     fn test_fn2_input() {
         scaffold_test(YEAR, DAY, "input.txt", "input-spec.2.txt", fn2);
     }

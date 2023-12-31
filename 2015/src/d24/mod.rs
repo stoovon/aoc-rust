98740d0644da6d07c2fbdfce265c4cafec452f14
Stuart Murray <10003354+stoovon@users.noreply.github.com>
2023-12-31 18:25:33 +0000

    feat: AoC2015d24

--- 2015/src/d24/mod.rs
+++ 2015/src/d24/mod.rs
@@ -1,11 +1,36 @@
 extern crate core;
 
-pub fn fn1(_input: &str) -> i64 {
-    todo!()
+use itertools::Itertools;
+
+fn entanglement(packages: Vec<i64>, divisor: i64) -> Option<i64> {
+    let target = packages.iter().sum::<i64>() / divisor;
+
+    let mut done = false;
+    let mut qe = i64::MAX;
+    for n in 1..packages.len() {
+        for combo in packages.iter().combinations(n) {
+            let sum: i64 = combo.iter().map(|x| *x).sum();
+            if sum == target {
+                qe = qe.min(combo.iter().map(|x| *x).product());
+                done = true;
+            }
+        }
+        if done {
+            return Some(qe);
+        }
+    }
+
+    None
 }
 
-pub fn fn2(_input: &str) -> i64 {
-    todo!()
+pub fn fn1(input: &str) -> i64 {
+    let packages: Vec<i64> = input.lines().map(|line| line.parse().unwrap()).collect();
+    entanglement(packages, 3).unwrap()
+}
+
+pub fn fn2(input: &str) -> i64 {
+    let packages: Vec<i64> = input.lines().map(|line| line.parse().unwrap()).collect();
+    entanglement(packages, 4).unwrap()
 }
 
 #[cfg(test)]
@@ -17,25 +42,11 @@ mod tests {
     const DAY: i16 = 24;
 
     #[test]
-    #[ignore]
-    fn test_fn1_example() {
-        scaffold_test(YEAR, DAY, "example.txt", "example-spec.1.txt", fn1);
-    }
-
-    #[test]
-    #[ignore]
     fn test_fn1_input() {
         scaffold_test(YEAR, DAY, "input.txt", "input-spec.1.txt", fn1);
     }
 
     #[test]
-    #[ignore]
-    fn test_fn2_example() {
-        scaffold_test(YEAR, DAY, "example.txt", "example-spec.2.txt", fn2);
-    }
-
-    #[test]
-    #[ignore]
     fn test_fn2_input() {
         scaffold_test(YEAR, DAY, "input.txt", "input-spec.2.txt", fn2);
     }
